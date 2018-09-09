e8a6a48ff6541f68b0ce45c38aba96a4238aa428
Busymeng <35614754+Busymeng@users.noreply.github.com>
2018-09-09 13:26:57 -0700

    Add files via upload

---
+++ ClassNote/Review 3_5 Answer.py
@@ -0,0 +1,23 @@
+###################################################################
+##  Review 3.5 Answers
+##
+
+1) c; 
+2) b; 
+3) d, the addition is attempted before conversion to an int; 
+4) c; 
+5) d; 
+6) c; 
+7) a; 
+8) b; 
+9) b; 
+10) a; 
+11) a; 
+12) b; 
+13) c, cannot add string and integer; 
+14) a; 
+15) d; 
+16) False; 
+17) True; 
+18) False, the argument must be a string; 
+19) True
