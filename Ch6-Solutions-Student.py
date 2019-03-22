d96beee735730be57eb8bc59daa491667650b4c3
Busymeng <35614754+Busymeng@users.noreply.github.com>
2019-03-21 22:39:52 -0700

    Add files via upload

---
+++ Ch6-Solutions-Student.py
@@ -0,0 +1,28 @@
+#####################################################################
+##  Ch6 Quiz
+##
+"""
+   1.   Hellothere
+   2.	   42
+   3.	   print(x[8])
+   4.	   print(x[14:17])
+   5.	   letter
+   6.   42
+   7.	   print(greet.upper())
+   8.	   shout() 
+   9.	   .ma
+   10.  strip()
+     
+"""
+
+#####################################################################
+##  Assignment 6.5
+##
+
+"""
+   * Write code using find() and string slicing (see section 6.10) to 
+     extract the number at the end of the line below. 
+     Convert the extracted value to a floating point number and print it out. 
+     
+"""
+text = "X-DSPAM-Confidence:    0.8475";
