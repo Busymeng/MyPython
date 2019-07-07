778e21607651d840f331a06b262e59c047d50301
Busymeng <35614754+Busymeng@users.noreply.github.com>
2019-07-06 22:07:45 -0700

    Add files via upload

---
+++ Project/projPopu.py
@@ -0,0 +1,24 @@
+'''Put overall header comments here.'''
+
+def open_file():
+    '''Insert function DocString here.'''
+    pass  # replace this line with your code.
+    
+def print_headers():
+    '''Insert function DocString here.'''
+    pass  # replace this line with your code.
+
+def calc_delta(line,col):
+    '''Insert function DocString here.'''
+    pass  # replace this line with your code.
+
+def format_display_line(continent,year,delta):
+    '''Insert function DocString here.'''
+    pass  # replace this line with your code.
+
+def main():
+    '''Insert function DocString here.'''
+    pass  # replace this line with your code.
+
+if __name__ == "__main__":
+    main()
