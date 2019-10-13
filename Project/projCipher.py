b73d28dd835857d4bcf01f133fe880f4a416ddd1
Busymeng <35614754+Busymeng@users.noreply.github.com>
2019-10-13 15:09:02 -0700

    Add files via upload

---
+++ Project/projCipher.py
@@ -0,0 +1,7 @@
+''' Insert heading comments here.'''
+
+answer = input("Would you like to (D)ecrypt, (E)ncrypt or (Q)uit? ")
+while answer.upper() != 'Q':
+     # Fill in the good stuff here instead of the following print
+    print("Testing:", answer)
+    answer = input("Would you like to (D)ecrypt, (E)ncrypt or (Q)uit? ")
