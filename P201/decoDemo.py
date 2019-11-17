6527a365abd6e152829f15d0f8557bec3d46e412
Busymeng <35614754+Busymeng@users.noreply.github.com>
2019-11-16 22:26:30 -0800

    Add files via upload

---
+++ P201/decoDemo.py
@@ -0,0 +1,15 @@
+#def do_twice(func):
+#    def wrapper_do_twice(*args,**kwargs):
+#        func(*args,**kwargs)
+#        func(*args,**kwargs)
+#    return wrapper_do_twice
+
+
+# For return value
+
+def do_twice(func):
+    def wrapper_do_twice(*args,**kwargs):
+        func(*args,**kwargs)
+        return func(*args,**kwargs)
+    return wrapper_do_twice
+
